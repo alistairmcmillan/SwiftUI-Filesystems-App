87bf88c8cab0b8f3ff25cfa3e103bc4c48be42e2
Alistair McMillan <alistairmcmillan75@gmail.com>
2024-05-16 21:10:12 +0100

    Initial Commit

---
+++ Filesystems/FilesystemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilesystemsApp.swift
+//  Filesystems
+//
+//  Created by Alistair McMillan on 16/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct FilesystemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
